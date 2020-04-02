550423db929a9f8359c6fc187590355586d4f154
Ryan Henderson <45128900+RyanEHenderson@users.noreply.github.com>
2020-04-02 14:38:49 -0500

    Added lava monitoring

--- src/main/java/me/roboticplayer/blockplacemonitor/BlockPlacementMonitor.java
+++ src/main/java/me/roboticplayer/blockplacemonitor/BlockPlacementMonitor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -13,15 +14,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.md_5.bungee.api.ChatColor;
 
 public class BlockPlacementMonitor extends JavaPlugin implements Listener {
 
 	private FileConfiguration config;
 	private int taskID;
 	private Map<UUID, Integer> trackerMap;
+	private Map<UUID, Integer> lavaMap;
 	private List<Material> blocked;
 
 	@Override
@@ -29,6 +30,7 @@ public class BlockPlacementMonitor extends JavaPlugin implements Listener {
 		config = this.getConfig();
 		saveDefaultConfig();
 		trackerMap = new HashMap<UUID, Integer>();
+		lavaMap = new HashMap<UUID, Integer>();
 		clearTimer();
 		populateMaterials();
 		getServer().getPluginManager().registerEvents(this, this);
@@ -70,6 +72,37 @@ public class BlockPlacementMonitor extends JavaPlugin implements Listener {
 
 	}
 
+	@EventHandler
+	public void onBucketEmpty(PlayerBucketEmptyEvent e) {
+		if (!config.getBoolean("monitorLava.enabled"))
+			return;
+		Player p = e.getPlayer();
+		int max = config.getInt("monitorLava.amount");
+		int remind = config.getInt("monitorLava.reminder");
+		if (e.isCancelled())
+			return;
+		if (p.hasPermission("blockmonitor.bypass"))
+			return;
+		if (!e.getBucket().toString().contains("LAVA"))
+			return;
+
+		if (lavaMap.containsKey(p.getUniqueId()))
+			lavaMap.put(p.getUniqueId(), lavaMap.get(p.getUniqueId()) + 1);
+		else
+			lavaMap.put(p.getUniqueId(), 1);
+		
+		if (lavaMap.get(p.getUniqueId()) >= max) {
+			if (lavaMap.get(p.getUniqueId()) == max || (lavaMap.get(p.getUniqueId()) - max) % remind == 0) {
+				for (Player staff : getServer().getOnlinePlayers()) {
+					if (staff.hasPermission("blockmonitor.notify")) {
+						staff.sendMessage(lavaMessage(p, e.getBlockClicked().getLocation()));
+					}
+				}
+				getLogger().info(lavaMessage(p, e.getBlockClicked().getLocation()));
+			}
+		}
+	}
+
 	private void populateMaterials() {
 		blocked = new ArrayList<Material>();
 		for (String mat : config.getStringList("blocks")) {
@@ -84,6 +117,7 @@ public class BlockPlacementMonitor extends JavaPlugin implements Listener {
 		taskID = getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 			public void run() {
 				trackerMap.clear();
+				lavaMap.clear();
 			}
 		}, config.getInt("cooldownTime") * 20, config.getInt("cooldownTime") * 20);
 	}
@@ -97,9 +131,21 @@ public class BlockPlacementMonitor extends JavaPlugin implements Listener {
 		return message;
 	}
 
+	private String lavaMessage(Player p, Location loc) {
+		String message = ChatColor.translateAlternateColorCodes('&', config.getString("monitorLava.message"));
+		message = message.replace("%PLAYER%", p.getName());
+		message = message.replace("%NUMBER%", String.valueOf(lavaMap.get(p.getUniqueId())));
+		message = message.replace("%TIME%", String.valueOf(config.getInt("cooldownTime")));
+		message = message.replace("%LOCATION%", formatLocation(loc));
+		return message;
+	}
+
 	private String formatLocation(Location loc) {
-		String message = "World: " + loc.getWorld().getName() + ", X: " + loc.getBlockX() + ", Y: " + loc.getBlockY()
-				+ ", Z: " + loc.getBlockZ();
+		String message = config.getString("locationFormat");
+		message = message.replace("%WORLD%", loc.getWorld().getName());
+		message = message.replace("%XCoord%", String.valueOf(loc.getBlockX()));
+		message = message.replace("%YCoord%", String.valueOf(loc.getBlockY()));
+		message = message.replace("%ZCoord%", String.valueOf(loc.getBlockZ()));
 
 		return message;
 	}
